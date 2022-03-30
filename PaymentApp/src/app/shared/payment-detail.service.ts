45f6b933b678d6691af8bf207cea2f36a86fbae2
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2022-03-31 01:51:42 +0300

    First_Commit

---
+++ PaymentApp/src/app/shared/payment-detail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { PaymentDetail } from './payment-detail.model';
+import {HttpClient} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+
+  constructor(private http:HttpClient) { }
+
+
+  formData:PaymentDetail = new PaymentDetail();
+  readonly baseURL = 'http://localhost:43897/api/paymentdetails';
+  postPaymentDetail(){
+    return this.http.post(this.baseURL,this.formData);
+  }
+}
